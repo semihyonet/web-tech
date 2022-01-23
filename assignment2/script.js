8c5e2ff0ccd9ccd172c2ccf22e4791ef1549007d
Semih Yonet <semihyonet@gmail.com>
2022-01-23 16:43:10 +0100

    Completed the needed changes on asignement

--- assignment2/script.js
+++ assignment2/script.js
@@ -4,7 +4,7 @@ function sortTable(n) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
   table = document.getElementById("table");
   switching = true;
-  dir = "asc"; 
+  dir = "asc";
   while (switching) {
     switching = false;
     rows = table.rows;
@@ -14,7 +14,7 @@ function sortTable(n) {
       y = rows[i + 1].getElementsByTagName("TD")[n];
       if (dir == "asc") {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          shouldSwitch= true;
+          shouldSwitch = true;
           break;
         }
       } else if (dir == "desc") {
@@ -27,7 +27,7 @@ function sortTable(n) {
     if (shouldSwitch) {
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
-      switchcount ++;      
+      switchcount++;
     } else {
       if (switchcount == 0 && dir == "asc") {
         dir = "desc";
@@ -37,36 +37,53 @@ function sortTable(n) {
   }
 }
 
-//task 2
-$(document).ready(function() {
-  $("#reset").click(function(){
-      $.ajax({
-          method: "GET",
-          url: "https://wt.ops.labs.vu.nl/api22/3ab02f27/reset",
-          dataType: "json"
-      });
-  });
-});
-
-//task 3
-$(document).ready(function(){
+const getAllItems = () => {
   $.ajax({
     method: "GET",
     url: "https://wt.ops.labs.vu.nl/api22/3ab02f27",
     dataType: "json"
-  }).done(function(data){
-    var content;
-    for (var i = 0; i < data.length; ++i){
+  }).done(function (data) {
+    var content = `
+    <table id="table">
+    <caption id="caption">Product List</caption>
+    <tr id="content">
+        <th>Image</th>
+        <th onclick="sortTable(1)">Brand</th>
+        <th onclick="sortTable(2)">Model</th>
+        <th onclick="sortTable(3)">OS</th>
+        <th onclick="sortTable(4)">Screensize</th>
+    </tr>`;
+    for (var i = 0; i < data.length; ++i) {
       content += "<tr>";
-      content += "<td><img alt=picture height=150 src=data[i].image/></td>";
+      content += `<td><img alt=picture height=150 src='${data[i].image}'/></td>`;
       content += "<td>" + data[i].brand + "</td>";
       content += "<td>" + data[i].model + "</td>";
       content += "<td>" + data[i].os + "</td>";
       content += "<td>" + data[i].screensize + "</td>";
       content += "</tr>";
     }
-    $("#table").append(content);
+    content += "</table>"
+    $("#table").replaceWith(content);
   });
+}
+
+
+//task 2
+$(document).ready(function () {
+  $("#reset").click(function () {
+    $.ajax({
+      method: "GET",
+      url: "https://wt.ops.labs.vu.nl/api22/3ab02f27/reset",
+      dataType: "json"
+    }).then(() => {
+      getAllItems()
+    });;
+  })
+});
+
+//task 3
+$(document).ready(function () {
+  getAllItems()
 });
 
 /*
@@ -82,14 +99,35 @@ $(document).ready(function(){
 });
 */
 
-//i use this function to see if array is updated
-$(document).ready(function(){
-  $.ajax({
-    url: "https://wt.ops.labs.vu.nl/api22/3ab02f27",
-    method: "GET",
-    dataType: "json"
-  }).done(function(data){
-    console.log(data);
-  });
+// Task 4
+$(document).ready(function () {
+  $("#submit").click(function () {
+    var data = {
+      image: $('#image').val(),
+      brand: $('#brand').val(),
+      model: $('#model').val(),
+      os: $('#os').val(),
+      screensize: $('#screensize').val()
+    }
+    $.ajax({
+      method: "POST",
+      url: "https://wt.ops.labs.vu.nl/api22/3ab02f27",
+      data,
+      dataType: "json"
+    }).then(() => {
+      getAllItems()
+    });;
+  })
 });
 
+
+//i use this function to see if array is updated
+// $(document).ready(function () {
+//   $.ajax({
+//     url: "https://wt.ops.labs.vu.nl/api22/3ab02f27",
+//     method: "GET",
+//     dataType: "json"
+//   }).done(function (data) {
+//     console.log(data);
+//   });
+// });
