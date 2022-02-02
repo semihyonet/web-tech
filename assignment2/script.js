a5081b4666dd51d70079a903d4bd8da14cd38364
unknown <pajarskas.matas@gmail.com>
2022-02-02 20:26:28 +0100

    matas part of the assignment

--- assignment2/script.js
+++ assignment2/script.js
@@ -110,3 +110,30 @@ $(document).ready(function () {
     });;
   })
 });
+
+var blink_speed = 500;
+var t = setInterval(function () {
+    var ele = document.getElementById('blinking');
+    ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
+}, blink_speed);
+
+
+var countDownDate = new Date("Feb 13, 2022 23:59:59").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("clock").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
